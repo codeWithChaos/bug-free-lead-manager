a518e864cb554100c3cd61420cae474378c51bcb
codewithchaos <codewithchaos1809@gmail.com>
2024-11-02 22:36:20 +0000

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+
+module.exports = {
+  mode: "development",
+  entry: "./frontend/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "frontend/static/frontend"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+      },
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  }
+};
